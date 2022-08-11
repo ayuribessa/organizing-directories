83e7bc0de63ef4e27ab05548e207fe4988b456be
Ayuri Bessa <ayuribessa@gmail.com>
2022-08-11 16:51:15 -0300

    Começando a colocar os assuntos dos livros

---
+++ script.py
@@ -0,0 +1,23 @@
+filetypes = {
+    "Images": ["png", "jpg", "jfif", "webp", "jpeg", "bmp", "tiff", "gif", "raw", "psd"],
+    "Documents": ["doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf", "txt"],
+    "Videos": ["mp4", "mpeg", "mkv", "srt"],
+    "Code": ["html", "css", "js", "py", "cpp", "c"],
+    "Audio": ["mp3", "wav", "ogg"],
+    "Compressed": ["zip", "tar", "rar"],
+}
+
+books_subject = {
+    "Math": ["Math", "Mathematics", "matemática"],
+    "Philosophy": ["Philosophy", "Filosofia", "moral", "ethics", "ética"],
+    "Biology": ["Biology", "Ecology", "Bio", "Cell", "Célula"],
+    "History": ["History", "História"],
+    "Engenharia": ["Engenharia", "engineering", "engineer"],
+    "Chemistry": [],
+    "Arte": ["Art", "Arte", "Draw", "Drawing"],
+    "Poesia": [],
+    "Science": ["Science", "ciência", "cientific"],
+    "Earth": [],
+    "Puzzles": [],
+    "Física": ["Physics", "Física"],
+}
